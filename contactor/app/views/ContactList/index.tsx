41ebe8d32f917d00298e801f43efba6d2012a0b6
Agnar Davíð Halldórsson <agnardavid@hotmail.com>
2024-12-03 16:05:30 +0000

    Services implemented

--- contactor/app/views/ContactList/index.tsx
+++ contactor/app/views/ContactList/index.tsx
@@ -3,12 +3,18 @@ import { View, Text } from "react-native";
 import { styles } from "./styles";
 import DisplayContactList from "@/app/components/ContactList";
 
+
+
+//const CONTACTS_DIRECTORY = 
+
 export const ContactListView = () => {
+  
   return (
     <View style={styles.container}>
       <DisplayContactList />
     </View>
   );
+
 };
 
 export default ContactListView;
