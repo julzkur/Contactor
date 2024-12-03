40f1893b577b7d58c546f7733ea41993724f5410
Agnar Davíð Halldórsson <agnardavid@hotmail.com>
2024-12-03 13:22:10 +0000

    Service template set up

--- contactor/app/models/contact.ts
+++ contactor/app/models/contact.ts
@@ -2,7 +2,28 @@ export interface Contact {
     id: string;
     name: string;
     phoneNumber: string;
-    thumbnail: string;
+    thumbnail?: string;
   }
+
+export class ContactModel {
+  constructor(
+    public name: string,
+    public phoneNumber: string,
+    public thumbnail?: string
+
+  ){}
   
+  toJson():string {
+    return JSON.stringify({
+      name: this.name,
+      phoneNumber: this.phoneNumber,
+      image: this.thumbnail || null,
+    });
+  }
+  fromJSON(json: string): ContactModel {
+    const data = JSON.parse(json);
+    return new ContactModel(data.name, data.phoneNumber, data.image);
+  }
+}
+
 export default Contact;
---
+++ contactor/app/services/ContactService.ts
@@ -0,0 +1,31 @@
+import { v4 as uuidv4 } from 'uuid';
+
+
+export const getAllContacts = (): string => {
+  return "";
+}
+
+export const getContactById = (): string => {
+  return "";
+}
+
+export const getContactByName = (): string => {
+  return "";
+}
+
+export const getContactByNumber = (): string => {
+  return "";
+}
+
+export const createContact = (): string => {
+  return "";
+}
+
+export const editContact = (): string => {
+  return "";
+}
+
+export const deleteContact = (): string => {
+  return "";
+}
+const id = uuidv4();
