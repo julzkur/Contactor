1bef2270d4c07d255450881dc43fae72c41186b9
dadimarpatti <dadip21@ru.is>
2024-12-03 14:33:53 +0000

    fixes

--- contactor/app/components/EditContact/index.tsx
+++ contactor/app/components/EditContact/index.tsx
@@ -7,7 +7,7 @@ const EditContact = () => {
       <View style={styles.container}>
         <TextInput placeholder="Name" style={styles.input} />
         <TextInput placeholder="Phone Number" style={styles.input} />
-        <TextInput placeholder="Email" style={styles.input} />
+        <TextInput placeholder="Thumbnail" style={styles.input} />
         <TouchableOpacity style={styles.button}>
           <Text style={styles.buttonText}>Save</Text>
         </TouchableOpacity>
